ab7cdc186304097d545a789098ba684c4a73b553
Andrew Kuchev <0coming.soon@gmail.com>
2014-12-01 15:22:44 +0500

    Added connection to DB

--- DjangoExample/settings.py
+++ DjangoExample/settings.py
@@ -57,12 +57,12 @@ WSGI_APPLICATION = 'DjangoExample.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
-        'USER': '',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'mysql.connector.django',
+        'NAME': 'DjangoExampleDB',
+        'USER': 'b973a1f9730056',
+        'PASSWORD': 'bad979f3',
+        'HOST': 'eu-cdbr-azure-north-b.cloudapp.net',
+        'PORT': '3306'
     }
 }
 
