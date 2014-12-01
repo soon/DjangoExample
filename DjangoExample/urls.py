1f5a79e804ce31c29a1395f6d0d99e1654752b29
Andrew Kuchev <0coming.soon@gmail.com>
2014-12-01 15:06:54 +0500

    So, this does not work

--- DjangoExample/urls.py
+++ DjangoExample/urls.py
@@ -6,5 +6,5 @@ urlpatterns = patterns('',
     # url(r'^$', 'DjangoExample.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
-    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^admin/', include(admin.site.urls)),
 )
